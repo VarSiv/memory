f2038ae961a4419828116007f596fd795b70e703
Varvara <var.mironov@gmail.com>
2023-07-11 20:41:42 -0300

    its ugly but it works

--- script.js
+++ script.js
@@ -16,18 +16,85 @@ function makeButtons(c) {
     document.getElementById("container").appendChild(card)
   }
 }
-makeButtons(buttons);
 
-
-const cardButtons = document.querySelectorAll('.card')
-cardButtons.forEach(button => {
-    button.addEventListener('click', () => {
-  
-      if (button.classList.contains('hidden')){
-        button.classList.remove('hidden')
-      }
-      else {
+class Game{
+  constructor (board, buttons, p1, p2, turns){
+    this.board = board
+    this.buttons = buttons
+    this.flipped1 = ''
+    this.flipped2 = ''
+    this.turn = 1
+    this.score1 = 0
+    this.score2 = 0
+    this.p1 = p1
+    this.p2 = p2
+    this.turns = turns
+  }
+  updateDisplay(){
+    const d1 = 'Player 1: '
+    const d2 = "Player 2: "
+    const t = 'Turn: '
+    this.p1.innerHTML = d1 + this.score1
+    this.p2.innerHTML = d2 + this.score2
+    this.turns.innerHTML = t + this.turn
+  }
+  hide(){
+    const cardButtons = document.querySelectorAll('.card')
+    cardButtons.forEach(button => {
+      if (! button.classList.contains('hidden')){
         button.classList.add('hidden')
+      }
+    })}
+  
+  
+  flip(){
+  let flips = 0
+  const cardButtons = document.querySelectorAll('.card')
+  cardButtons.forEach(button => {
+    button.addEventListener('click', () => {
+      if (button.classList.contains('hidden')){
+        flips += 1
+        button.classList.remove('hidden')
+        if (this.flipped1===''){
+          this.flipped1=button
+        }
+        else if (this.flipped2===''){
+          this.flipped2=button
+        }
+      }
+      if (flips > 2){
+        this.hide()
+        flips = 0
+        if (this.flipped1.classList[1]==this.flipped2.classList[1]){
+          this.flipped1.classList.add('matched')
+          this.flipped2.classList.add('matched')
+          if (this.turn%2==1){
+            this.score1 +=1
+          }
+          else {
+            this.score2 += 1
+          }
+        }
+        else {
+          this.turn += 1
+        }
+        this.flipped1=''
+        this.flipped2=''
+        this.updateDisplay()
       }
     })
   })
+}
+    
+  }
+
+
+makeButtons(buttons);
+const cardButtons = document.querySelectorAll('.card')
+const cardContainer = document.querySelector('.cards-contaoner')
+const player1 = document.querySelector('.score-1')
+const player2 = document.querySelector('.score-2')
+const turnCounter = document.querySelector('.turns')
+const myGame = new Game (cardContainer, cardButtons, player1, player2, turnCounter)
+myGame.flip()
+myGame.updateDisplay()
